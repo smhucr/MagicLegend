0fc7667050c0e53a7d54b46b447589a16bd0d663
Semih Uçar <smhucr10@gmail.com>
2023-12-14 16:00:19 +0300

    DeletedCodesImportAgain

--- MagicLegend/Assets/Scripts/GameManager.cs
+++ MagicLegend/Assets/Scripts/GameManager.cs
@@ -5,4 +5,20 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
+
+    [Header("Player Settings")]
+    public float playerSpeed = 5f;
+    public DynamicJoystick joystick; 
+    public Rigidbody rb;
+
+    private void MakeInstance()
+    {
+        if (instance == null)
+            instance = this;
+    }
+    private void Awake()
+    {
+        MakeInstance();
+        //DontDestroyOnLoad(gameObject);
+    }
 }
---
+++ MagicLegend/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Transform playerMeshObject; 
+
+    private DynamicJoystick joystick;
+    private Rigidbody rb;
+    [SerializeField] private float moveSpeed = 5f;
+
+    private void Start()
+    {
+        moveSpeed = GameManager.instance.playerSpeed;
+        rb = GameManager.instance.rb;
+        joystick = GameManager.instance.joystick;
+    }
+
+    private void FixedUpdate()
+    {
+        MovePlayer();
+    }
+
+    private void MovePlayer()
+    {
+        Vector3 movement = new Vector3(joystick.Horizontal, 0f, joystick.Vertical).normalized;
+
+        if (movement != Vector3.zero)
+        {
+            Vector3 moveVector = new Vector3(movement.x, 0f, movement.z) * moveSpeed * Time.fixedDeltaTime;
+            rb.MovePosition(transform.position + moveVector);
+
+            if (playerMeshObject != null)
+            {
+                playerMeshObject.transform.forward = moveVector.normalized;
+            }
+        }
+    }
+}
